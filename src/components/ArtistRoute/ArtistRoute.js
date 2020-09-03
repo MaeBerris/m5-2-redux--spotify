78c4a4ff0433183c9541482e46addde3b5b95afc
Mae <maeberris@outlook.com>
2020-09-02 23:42:58 -0400

    completed exerise 7

--- src/components/ArtistRoute/ArtistRoute.js
+++ src/components/ArtistRoute/ArtistRoute.js
@@ -1,14 +1,58 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { fetchArtistProfile } from "../../helpers/api-helpers";
+import {
+  requestArtistProfile,
+  receiveArtistProfile,
+  receiveArtistProfileError,
+} from "../../action";
+import { useDispatch } from "react-redux";
+import approximateNumber from "approximate-number";
 
 const ArtistRoute = () => {
   const { id } = useParams();
   const accessToken = useSelector((state) => state.auth.token);
+  const {
+    artists: { currentArtist, status },
+  } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  let slicedArray = [];
+  if (currentArtist !== null) {
+    slicedArray = currentArtist.profile.genres.slice(0, 2);
+  }
+
+  React.useEffect(() => {
+    if (!accessToken) {
+      return;
+    }
+    dispatch(requestArtistProfile());
+    fetchArtistProfile(accessToken, id)
+      .then((data) => {
+        dispatch(receiveArtistProfile(data));
+      })
+      .catch((err) => {
+        dispatch(receiveArtistProfileError());
+      });
+  }, [accessToken]);
+
+  if (status === "loading" || currentArtist === null) {
+    return <div>loading</div>;
+  }
+
   return (
     <div>
-      ID : {id}
-      Token : {accessToken}
+      <header>
+        <img src={currentArtist.profile.images[1].url} />
+        <h1>{currentArtist.profile.name}</h1>
+      </header>
+      <p>{approximateNumber(currentArtist.profile.followers.total)}</p>
+      <h2>Tags</h2>
+      <ul>
+        {slicedArray.map((item) => {
+          return <li>{item}</li>;
+        })}
+      </ul>
     </div>
   );
 };
--- src/reducers/artists-reducer.js
+++ src/reducers/artists-reducer.js
@@ -5,6 +5,26 @@ const initialState = {
 
 export default function artistsReducer(state = initialState, action) {
   switch (action.type) {
+    case "REQUEST_ARTIST_PROFILE": {
+      return {
+        ...state,
+        status: "loading",
+      };
+    }
+    case "RECEIVE_ARTIST_PROFILE": {
+      return {
+        currentArtist: {
+          profile: action.profile,
+        },
+        status: "idle",
+      };
+    }
+    case "RECEIVE_ARTIST_PROFILE_ERROR": {
+      return {
+        ...state,
+        status: "error",
+      };
+    }
     default: {
       return state;
     }
